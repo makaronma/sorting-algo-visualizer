5890e4fe8d89b683e797965b0befc1d67341cd10
makaronma <mckahung250@gmail.com>
2022-02-16 00:56:26 +0800

    fixed dataContainers over-rerender problem

--- src/components/DataDisplayBoard.js
+++ src/components/DataDisplayBoard.js
@@ -1,14 +1,13 @@
 import React from "react";
 import DataContainer from "./DataContainer";
-import { v4 as uuidv4 } from 'uuid';
 
 const DataDisplayBoard = ({ dataset }) => {
   const numOfData = dataset.length;
 
   return (
     <div id="dataDisplayBoard">
-      {dataset.map((data) => (
-        <DataContainer data={data} numOfData={numOfData} key={uuidv4()}/>
+      {dataset.map((data,index) => (
+        <DataContainer data={data} numOfData={numOfData} key={`data-${index}`}/>
       ))}
     </div>
   );
